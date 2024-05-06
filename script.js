1bf9d913746687134ce2a646390d7c68068cc191
Juan Pablo <jpcorvo219@gmail.com>
2024-05-06 18:19:01 -0300

    minor correction

--- script.js
+++ script.js
@@ -4,14 +4,11 @@ L.tileLayer('https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=th1B7ltI
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
 }).addTo(map);
 
-
-
 function getData() {
     const response = fetch('./liceos.json')
         .then(response => response.json())
         .then(data => createMarker(data))
 }
-
 
 function createMarker(data) {
     data["liceos"].forEach(object => {
@@ -20,9 +17,6 @@ function createMarker(data) {
 
 }
 
-
-
 document.addEventListener("DOMContentLoaded", () => {
     getData()
-
 })
